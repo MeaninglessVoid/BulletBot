7229db40e8599b5833102a39ac3bfa48fbc95a37
SamuelDub <dubnersamuel@yahoo.com>
2018-01-22 09:32:41 -0800

    create giveaway category

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -4,37 +4,44 @@ module.exports = {
     message.reply(
       "This command is being rewritten, please refrain from using it"
     );
-    // if (
-    //   message.member.roles.has("306234174321328129") || //check if owner
-    //   message.author.id == "340002869912666114" //check if void
-    // ) {
-    //   let argsL = message.content.split(" ").slice(1);
-    //   if (argsL[0] == undefined) {
-    //     var embed = new Discord.RichEmbed()
-    //       .setAuthor("Giveaway Help", message.author.displayAvatarURL)
-    //       .setColor("#9400ff")
-    //       .setThumbnail(bot.user.displayAvatarURL)
-    //       .setTimestamp()
-    //       .addField(
-    //         "!giveaway start",
-    //         "This command will start a new giveaway, and create the necessary channels!"
-    //       )
-    //       .addField(
-    //         "!giveaway choose",
-    //         "This command will choose a winner, and add them to the winner vc!"
-    //       )
-    //       .addField(
-    //         "!giveaway end",
-    //         "This command will end the giveaway and delete all the channels!"
-    //       );
-    //   } else if (argsL[0].toLowerCase() == "start") {
-    //   } else if (argsL[0].toLowerCase() == "choose") {
-    //   } else if (argsL[0].toLowerCase() == "end") {
-    //   }
-    // } else {
-    //   message.reply("You don't have permission to use this command.");
-    // }
-    // message.delete();
+    return; //remove this later
+    if (
+      message.member.roles.has("306234174321328129") || //check if owner
+      message.author.id == "340002869912666114" //check if void
+    ) {
+      let argsL = message.content.split(" ").slice(1);
+      if (argsL[0] == undefined) {
+        var embed = new Discord.RichEmbed()
+          .setAuthor("Giveaway Help", message.author.displayAvatarURL)
+          .setColor("#9400ff")
+          .setThumbnail(bot.user.displayAvatarURL)
+          .setTimestamp()
+          .addField(
+            "!giveaway start",
+            "This command will start a new giveaway, and create the necessary channels!"
+          )
+          .addField(
+            "!giveaway choose",
+            "This command will choose a winner, and add them to the winner vc!"
+          )
+          .addField(
+            "!giveaway end",
+            "This command will end the giveaway and delete all the channels!"
+          );
+      } else if (argsL[0].toLowerCase() == "start") {
+        message.guild.createChannel("🎉 • Giveaway •", "category", [
+          {
+            id: message.guild.roles.get("306234601817505793"),
+            allow: ["VIEW_CHANNEL", "CONNECT", "SPEAK"],
+          }
+        ]);
+      } else if (argsL[0].toLowerCase() == "choose") {
+      } else if (argsL[0].toLowerCase() == "end") {
+      }
+    } else {
+      message.reply("You don't have permission to use this command.");
+    }
+    message.delete();
   }
 };
 
