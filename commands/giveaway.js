391cddebab24a81a9d7a6b594c2fc60e95f6df16
SamuelDub <dubnersamuel@yahoo.com>
2018-01-22 09:56:44 -0800

    create necessary channels

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -85,56 +85,7 @@ module.exports = {
 //     ) {
 //       let argsL = message.content.split(" ").slice(1);
 //       if (argsL[0] == undefined) {
-//         message.author.send(
-//           "Please use either `!giveaway start` to start the giveaway, `!giveaway choose` to choose a lucky winner, or `!giveaway end` to end the giveaway and delete both channels"
-//         );
 //       } else if (argsL[0].toLowerCase() == "start") {
-//         message.guild
-//           .createChannel("🎉 GIVEAWAY!", "category")
-//           .then(channel => {
-//             channel.setPosition(3);
-//             channel.overwritePermissions(
-//               message.guild.roles.get("306234601817505793"),
-//               {
-//                 VIEW_CHANNEL: true,
-//                 CONNECT: true
-//               }
-//             );
-//           });
-
-//         message.guild
-//           .createChannel("Giveaway Entry!", "voice")
-//           .then(channel => {
-//             channel.overwritePermissions(
-//               message.guild.roles.get("306234601817505793"),
-//               {
-//                 VIEW_CHANNEL: true,
-//                 CONNECT: true
-//               }
-//             );
-//           });
-
-//         message.guild
-//           .createChannel("Giveaway Winners!", "voice")
-//           .then(channel => {
-//             channel.overwritePermissions(
-//               message.guild.roles.get("306234601817505793"),
-//               {
-//                 VIEW_CHANNEL: true,
-//                 CONNECT: false,
-//                 SPEAK: true
-//               }
-//             );
-//           });
-
-//         setTimeout(function() {
-//           message.guild.channels
-//             .find("name", "Giveaway Entry!")
-//             .setParent(message.guild.channels.find("name", "🎉 GIVEAWAY!"));
-//           message.guild.channels
-//             .find("name", "Giveaway Winners!")
-//             .setParent(message.guild.channels.find("name", "🎉 GIVEAWAY!"));
-//         }, 1000);
 //       } else if (argsL[0].toLowerCase() == "choose") {
 //         var winner = message.guild.channels
 //           .find("name", "Giveaway Entry!")
